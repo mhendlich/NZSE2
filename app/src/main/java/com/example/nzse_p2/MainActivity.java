cae229c0d9d53d591a5052b4b4682827b278ecf5
Steffen Uhlenbrock <steffen.uhlenbrock@stud.h-da.de>
2020-12-17 18:41:19 +0100

    "Sign In"-Button funktioniert nun mit mail:"test" pw:"1234" (auskommentiert!)

--- app/src/main/java/com/example/nzse_p2/MainActivity.java
+++ app/src/main/java/com/example/nzse_p2/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -18,11 +19,20 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         Button btnSignIn = (Button) findViewById(R.id.btnSignIn);
+
         btnSignIn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
-                //perform action on click
-                Intent activityChangeIntent = new Intent(MainActivity.this, overviewScreen.class);
-                MainActivity.this.startActivity(activityChangeIntent);
+                EditText editTextMail = (EditText) findViewById(R.id.editTextTextEmailAddress);
+                String stringMail = editTextMail.getText().toString();
+
+                EditText editTextPW = (EditText) findViewById(R.id.editTextTextPassword);
+                String stringPW = editTextPW.getText().toString();
+
+//                if(stringMail.equals("test") && stringPW.equals("1234")){
+                    //perform action on click
+                    Intent activityChangeIntent = new Intent(MainActivity.this, overviewScreen.class);
+                    MainActivity.this.startActivity(activityChangeIntent);
+//                }
             }
         });
     }
