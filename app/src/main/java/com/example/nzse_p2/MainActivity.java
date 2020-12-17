71045dc4f1ebc54f5f885a4fdaeb5d45539e554b
Steffen Uhlenbrock <steffen.uhlenbrock@stud.h-da.de>
2020-12-17 18:02:21 +0100

    "Sign In"-Button ruft neue Activity auf ohne Mail/PW check

--- app/src/main/java/com/example/nzse_p2/MainActivity.java
+++ app/src/main/java/com/example/nzse_p2/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.nzse_p2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -13,5 +16,16 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        Button btnSignIn = (Button) findViewById(R.id.btnSignIn);
+        btnSignIn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                //perform action on click
+                Intent activityChangeIntent = new Intent(MainActivity.this, overviewScreen.class);
+                MainActivity.this.startActivity(activityChangeIntent);
+            }
+        });
     }
+
+
 }
