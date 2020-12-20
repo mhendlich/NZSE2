e1d72590d0b539dbc5b37b77559151692b2d848b
Steffen <steffen.uhlenbrock@stud.h-da.de>
2020-12-20 13:06:30 +0100

    byebye standard-actionbar

--- app/src/main/java/com/example/nzse_steffen_michael/overviewScreen.java
+++ app/src/main/java/com/example/nzse_steffen_michael/overviewScreen.java
@@ -3,6 +3,7 @@ package com.example.nzse_steffen_michael;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class overviewScreen extends AppCompatActivity {
 
